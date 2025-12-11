447b9cb5a5fe43bdbdde9c60ef7443055c9a6c81
Formando <Skills@FFLG203-1.centro.cfpic.pt>
2025-12-11 15:37:47 +0000

    Atividade4

--- Atividade4/script.js
+++ Atividade4/script.js
@@ -1,79 +1,37 @@
-function verificar() {
-    var data = new Date()
-    var ano = data.getFullYear()
-    var fano = document.getElementById('txtano')
-    var res = document.querySelector('#res')
+function contar(){
+    var ini = document.getElementById('txti')
+    var fim = document.getElementById('txtf')
+    var passo = document.getElementById('txtp')
+    var res = document.getElementById('res')
 
-    if (fano.value.length == 0 || Number(fano.value) > ano) {
-        window.alert('[Erro] Verifique os dados e tente novamente!')
-    } else {
-        var fsex = document.getElementsByName('radsex')
-        var idade = ano - Number(fano.value)
-        var genero = ''
-        var img = document.createElement('img')
-        img.setAttribute('id', 'foto')
-
-        if (fsex[0].checked) {
-            genero = 'Homem'
-
-            if (idade < 2) {
-                img.setAttribute('src', 'img/bebemenino1.jpg')
-                // Bebê
-                genero = 'Bebe'
-            } else if (idade < 10) {
-                // Menino
-                genero = 'Menino'
-                img.setAttribute('src', 'img/menino.jpg')
-            } else if (idade < 18) {
-                // Adolescente
-                genero = 'Adolescente'
-                img.setAttribute('src', 'img/teenagerboy.jpg')
-            } else if (idade < 30) {
-                // Jovem
-                genero = 'Jovem'
-                img.setAttribute('src', 'img/homemjovem.jpg')
-            } else if (idade < 50) {
-                // Adulto
-                genero = 'Adulto'
-                img.setAttribute('src', 'img/Homemadulto.jpg')
-            } else {
-                // Idoso
-                genero = 'Idoso'
-                img.setAttribute('src', 'img/Idoso.jpg')
-            }
-
-        } else if (fsex[1].checked) {
-            genero = 'Mulher'
-
-            if (idade < 2) {
-                // Bebê
-                genero = 'Bebe'
-                img.setAttribute('src', 'img/bebemenina.jpg')
-            } else if (idade < 10) {
-                // Menino
-                genero = 'Menina'
-                img.setAttribute('src', 'img/menina.jpg')
-            } else if (idade < 18) {
-                // Adolescente
-                genero = 'Adolescente'
-                img.setAttribute('src', 'img/teenage.jpg')
-            } else if (idade < 30) {
-                // Jovem
-                genero = 'Jovem'
-                img.setAttribute('src', 'img/MulherJovem.jpg')
-            } else if (idade < 50) {
-                // Adulto
-                genero = 'Adulta'
-                img.setAttribute('src', 'img/MulherAdulta.jpg')
-            } else {
-                // Idosa
-                genero = 'Idosa'
-                img.setAttribute('src', 'img/idosa.jpg')
-            }
+    if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
+        //window.alert('[ERRO] Faltam dados!')
+        res.innerHTML = 'Impossível contar!'
+    } else{
+        res.innerHTML = 'Contando: '
+        var i = Number(ini.value)
+        var f = Number(fim.value)
+        var p = Number(passo.value)
+        if (p <= 0){
+            //window.alert('Passo inválido! Considerando PASSO 1')
+            res.innerHTML = 'Passo irá para 1:: '
+            p = 1
         }
-
-        res.style.textAlign = 'center'
-        res.innerHTML = `Detectamos ${genero} com ${idade} anos.`
-        res.appendChild(img)
+        if ( i<f){
+            //Contagem Crescente
+            for (var c = i; c <= f; c += p){
+                res.innerHTML += `${c} \u{1F449}` 
+            }
+        } else {
+            //Contagem Decrescente
+            for (var c = i; c >= f; c -= p){
+                res.innerHTML += `${c} \u{1F449}`
+        }
     }
+    res.innerHTML += `\u{1F3C1}`
+}
+
+
+
+
 }
